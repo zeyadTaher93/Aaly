8973af53fce494452e5972f6c4c9eb41583b455f
Zeyad <zeyadtaher93@gmail.com>
2019-12-05 12:42:59 +0200

    Initial Commit

---
+++ Aley/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aley
+//
+//  Created by zeyad on 12/5/19.
+//  Copyright © 2019 zeyad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
